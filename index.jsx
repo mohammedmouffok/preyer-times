a4916189602be77feee1bec9681af6c9473a279b
mahdi <mouffokmed5@gmail.com>
2026-06-22 12:30:19 +0100

    Updates

---
+++ index.jsx
@@ -0,0 +1,5 @@
+export { default as Fajr } from "/picture/Fajr";
+export { default as Dhuhr } from "/picture/Dhuhr";
+export { default as Asr } from "/picture/Asr";
+export { default as Maghrib } from "/picture/Maghrib";
+export { default as Isha } from "/picture/Isha";
